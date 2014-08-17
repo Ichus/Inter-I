7b0f798c70d266b52e7e529ed7fe05c11bd56077
Ichus <Ichus.local>
2014-08-17 05:21:55 -0400

    Basic structure for categories in. Need to fix routes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   def show
     @idea = params[:idea]
     @relations = find_relations(@idea)
+    @categories = find_categories(@idea)
   end
 
   def find_relations(idea)
@@ -20,7 +21,27 @@ class HomeController < ApplicationController
     end
   end
 
+  def find_categories(idea)
+    categories = []
+    nodey = Neo4j::Node.find("idea: #{idea.inspect}~")
+    if nodey.first
+      node = nodey.first
+      nodey.close
+      node.rels.each do |rel|
+        category = rel["category"]
+        category_flag = category.chars.first.to_i
+        category = category.slice(1..-1)
+        already_found = false
+        categories.each { |cat| already_found = true if cat[1].eql? category }
+        categories << [category_flag, category] unless already_found
+      end
+      categories
+    else
+      false
+    end
+  end
+
   def home_params
-    params.permit(:idea)
+    params.permit(:idea, :category)
   end
 end
