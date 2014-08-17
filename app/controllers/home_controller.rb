a8483d6e123fb3d27714befe2e61749409b5ddd5
Ichus <Ichus.local>
2014-08-17 00:33:32 -0400

    Basic Relations working. Problem with home#show route though.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,24 @@ class HomeController < ApplicationController
   end
 
   def show
+    @idea = params[:idea]
+    @relations = find_relations(@idea)
+  end
+
+  def find_relations(idea)
+    nodey = Neo4j::Node.find("idea: #{idea.inspect}")
+    if nodey.first
+      node = nodey.first
+      nodey.close
+      relations = []
+      node.outgoing(:relation).filter{|path| path.relationships.first[:weight] > 0.7515 && !/#{node[:idea]}/i.match(path.end_node[:idea]) }.each {|rel| relations << rel[:idea] }
+      relations
+    else
+      false
+    end
+  end
+
+  def home_params
+    params.permit(:idea)
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def relations?(idea)
+    nodey = Neo4j::Node.find("idea: #{idea.inspect}")
+    if nodey.first
+      node = nodey.first
+      nodey.close
+      relations = []
+      node.outgoing(:relation).filter{|path| path.relationships.first[:weight] > 0.7515 && !/#{node[:idea]}/i.match(path.end_node[:idea]) }.each {|rel| relations << rel[:idea] }
+      !!relations.first
+    end
+  end
 end
