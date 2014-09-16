a308ca1c19f24f3a06035f13cc1c6dd33d243d73
Ichus <Ichus.local>
2014-09-16 13:35:34 -0700

    Fixed Show Template Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'relationships/:id' => 'idea_relations#show', :as => 'show_relations'
-  get 'relationships/:id/:category' => 'idea_relations#show', :as => 'show_categorical_relations'
+  get 'relationships/:idea' => 'idea_relations#show', :as => 'show_relations'
+  get 'relationships/:idea/:category' => 'idea_relations#show', :as => 'show_categorical_relations'
 
   root 'idea_relations#index'
 end
