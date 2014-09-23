c2dd07f062a31a5b4144f5fafe5353c5b796537c
Ichus <Ichus.local>
2014-09-23 02:07:17 -0700

    JS to swap 'Explore Ideas' search button with formaction containing searched for path Now uses Absolute Path

--- app/assets/javascripts/idea_relations_update_search_path.js
+++ app/assets/javascripts/idea_relations_update_search_path.js
@@ -1,5 +1,20 @@
+// Swap out the Explore Ideas Search button for one with a formaction containing
+// the Absolute Path of the idea being searched
 $( "#idea" )
 .change(function () {
   var ideaPath = $( this ).val();
-  $( "<span id=\"explore-ideas\"><input class=\"tiny button blue-button\" formaction=\"/relationships/" + ideaPath + "\" type=\"submit\" value=\"Explore Ideas\" /></span>" ).replaceAll( "#explore-ideas" );
+  var pageUri = location.href;
+  var hostRegEx = /http:\/\/[^\/]*/;
+  var hostMatchArray = hostRegEx.exec(pageUri);
+  var hostUri = hostMatchArray[0].toString();
+  $( "<span id=\"explore-ideas\"><input class=\"tiny button blue-button\" formaction=\"" + hostUri + "/relationships/" + ideaPath + "\" type=\"submit\" value=\"Explore Ideas\" /></span>" ).replaceAll( "#explore-ideas" );
 });
+
+
+// Swap out the Explore Ideas Search button for one with a formaction containing
+// the Relative Path of the idea being searched
+// $( "#idea" )
+// .change(function () {
+//   var ideaPath = $( this ).val();
+//   $( "<span id=\"explore-ideas\"><input class=\"tiny button blue-button\" formaction=\"/relationships/" + ideaPath + "\" type=\"submit\" value=\"Explore Ideas\" /></span>" ).replaceAll( "#explore-ideas" );
+// });
